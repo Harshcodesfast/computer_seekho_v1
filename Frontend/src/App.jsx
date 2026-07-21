acbffefc61e349dcfd5b91293e9983da4858ccc8
Pratik H <pratikhagawane745@gmail.com>
2026-07-21 20:03:06 +0530

    init

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import api from "./services/api";
+
+function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+
+    api.get("/health")
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }, []);
+
+  return (
+    <div>
+
+      <h1>Computer Seekho</h1>
+
+      <h2>{message}</h2>
+
+    </div>
+  );
+}
+
+export default App;
+//hello
